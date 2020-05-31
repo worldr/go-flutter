160eb90a6daaccb6a5520e3771770db8a45593c8
jld3103 <jld3103yt@gmail.com>
2020-05-31 22:29:39 +0200

    TEST: remove CGO LDFLAGS

--- embedder/build.go
+++ embedder/build.go
@@ -1,6 +1,3 @@
 package embedder
 
-// #cgo linux LDFLAGS: -lflutter_engine -Wl,-rpath,$ORIGIN
-// #cgo windows LDFLAGS: -lflutter_engine
-// #cgo darwin LDFLAGS: -framework FlutterEmbedder
 import "C"
